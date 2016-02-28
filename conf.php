77f0103cf2caa9f6d2d8ad890061a0b46f0464be
XenK0u <x@x.xxx>
2016-02-28 14:52:37 +0800

    add conf tips

--- conf.php
+++ conf.php
@@ -5,17 +5,13 @@
  * @date    2016-02-28
  */
 
-define('U', '马赛克');
-define('P', '马赛克');
-define('CLINENTIP', '马赛克');
+define('U', '马赛克');//your account
+define('P', '马赛克');//your password
+define('CLINENTIP', '马赛克');//your ip
 define('NASIP', '219.128.230.1');
 //mac eg: FF-FF-FF-FF-FF-FF
-define('MAC', '马赛克');
-define('TIME', getTimestamp());
+define('MAC', '马赛克');//your mac address
+define('TIME', define('TIME', number_format(microtime(true),3,'','')););
 define('WIFI', '1050');
 define('WIFI2', '4060');
 define('SECRET', 'Eshore!@#');
-
-function getTimestamp() {
-return number_format(microtime(true),3,'','');
-}
