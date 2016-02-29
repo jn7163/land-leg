499a5ccab262cb61515e2ab6c2060ea48038020c
XenK0u <x@x.xxx>
2016-02-29 18:42:10 +0800

    fix error

--- conf.php
+++ conf.php
@@ -11,7 +11,7 @@ define('CLINENTIP', '马赛克');//your ip
 define('NASIP', '219.128.230.1');
 //mac eg: FF-FF-FF-FF-FF-FF
 define('MAC', '马赛克');//your mac address
-define('TIME', define('TIME', number_format(microtime(true),3,'','')););
+define('TIME', number_format(microtime(true),3,'',''));
 define('WIFI', '1050');
 define('WIFI2', '4060');
 define('SECRET', 'Eshore!@#');
